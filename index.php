994cf1738c67c6b1e1610f2e69484dd16e719980
josenorth <josesitovcf@gmail.com>
2024-09-09 22:13:39 -0300

    Index

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ $isLoggedIn = isset($_SESSION['user_id']);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
     <link rel="stylesheet" href="/public/assets/css/style.css">
     <link rel="stylesheet" href="/public/assets/css/nav-blur.css">
+    <link rel="stylesheet" href="/public/assets/css/footer.css">
     <style>
         .overlay {
             display: none;
@@ -146,35 +147,35 @@ $isLoggedIn = isset($_SESSION['user_id']);
         </div>
     </div>
     <?php if (isset($_GET['accountDeleted']) && $_GET['accountDeleted'] == 'true'): ?>
-    <div class="modal" id="accountDeletedModal">
-        <div class="modal-content">
-            <h2>Cuenta eliminada</h2>
-            <p>Tu cuenta ha sido eliminada exitosamente.</p>
-            <button onclick="closeModalAccount()">Cerrar</button>
+        <div class="modal" id="accountDeletedModal">
+            <div class="modal-content">
+                <h2>Cuenta eliminada</h2>
+                <p>Tu cuenta ha sido eliminada exitosamente.</p>
+                <button onclick="closeModalAccount()">Cerrar</button>
+            </div>
         </div>
-    </div>
-    <script>
-        function closeModalAccount() {
-            document.getElementById('accountDeletedModal').style.display = 'none';
+        <script>
+            function closeModalAccount() {
+                document.getElementById('accountDeletedModal').style.display = 'none';
 
-            // Actualiza la URL sin el parámetro 'accountDeleted'
-            var url = new URL(window.location.href);
-            url.searchParams.delete('accountDeleted');
-            window.history.replaceState({}, '', url);
-        }
+                // Actualiza la URL sin el parámetro 'accountDeleted'
+                var url = new URL(window.location.href);
+                url.searchParams.delete('accountDeleted');
+                window.history.replaceState({}, '', url);
+            }
 
-        // Mostrar el modal
-        document.getElementById('accountDeletedModal').style.display = 'block';
-    </script>
-<?php endif; ?>
+            // Mostrar el modal
+            document.getElementById('accountDeletedModal').style.display = 'block';
+        </script>
+    <?php endif; ?>
     <header>
         <?php include 'public/templates/nav-blur.php' ?>
         <div class="carousel-content">
             <h1>RECIEN HECHO, TODOS LOS DIAS</h1>
             <p>Café recién preparado con granos seleccionados para ofrecerte una experiencia inigualable</p>
             <div class="buttons">
-                <a href="/public/reservas.php" class="btn-order">Reserva una mesa</a>
-                <a href="/public/tienda.php" class="btn-menu">Ver Menú</a>
+                <a href="#order" class="btn-order">Order Now</a>
+                <a href="#menu" class="btn-menu">View Menu</a>
             </div>
         </div>
         <div class="carousel">
@@ -195,7 +196,43 @@ $isLoggedIn = isset($_SESSION['user_id']);
         </div>
     </header>
     <main>
+    <section class="info">
+        <div class="info-box">
+            <h3>Ven al brunch de los domingos</h3>
+            <p>Disfruta de nuestro delicioso brunch dominical en un ambiente acogedor, con café fresco y una selección
+                de platillos para empezar bien tu día.</p>
+        </div>
+        <div class="info-box">
+            <h3>Celebra tus fiestas con nosotros</h3>
+            <p>Organiza tus eventos y celebraciones con nosotros, garantizando un ambiente acogedor y sabores irresistibles.</p>
+        </div>
+    </section>
+    <section class="cta">
+        <div class="cta-content">
+            <h3>Ven desayunar</h3>
+            <p>Medialunas, Croissants, Jugos o lo que quieras. Aquí en Café Sabrosos.</p>
+            <button onclick="location.href='/public/tienda.php'">Descubre nuestros productos<i class="fa-solid fa-mug-saucer"></i></i></button>
+        </div>
+    </section>
+    <section class="features">
+        <div class="feature">
+            <h4>Café recién hecho</h4>
+            <p>Café recién molido y preparado con esmero para ofrecerte la mejor experiencia.</p>
+        </div>
+        <div class="feature">
+            <h4>Tartas para cada día</h4>
+            <p>Variedad de tartas frescas y deliciosas disponibles todos los días para satisfacer tu antojo.</p>
+        </div>
+        <div class="feature">
+            <h4>Prueba nuestros cafés</h4>
+            <p>Explora nuestra selección de cafés únicos, preparados por expertos baristas.</p>
+        </div>
+        <div class="feature">
+            <h4>Lo que busques</h4>
+            <p>Nos adaptamos a tus gustos y preferencias para brindarte exactamente lo que deseas.</p>
+        </div>
     </main>
+    <?php include 'public/templates/footer.php'; ?>
     <script src="/public/assets/js/nose.js"></script>
     <script>
         function closeModal() {
